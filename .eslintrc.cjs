81c45c4382368c987ecc2f3837ca1bce0d0b4468
阿逼 <2553400824@qq.com>
2024-08-12 01:09:27 +0800

    chore(changelog.config.cjs):提交规范

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,17 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
+  env: {
+    es2021: true,
+    node: true,
+  },
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting' // vue 放弃 eslint的代码格式化，交接给prettier
+    '@vue/eslint-config-prettier/skip-formatting', // vue 放弃 eslint的代码格式化，交接给prettier
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 2023,
+  },
 }
---
+++ changelog.config.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  // 指定预设
+  preset: 'angular',
+  // 自定义解析器选项
+  parserOpts: {
+    headerPattern: /^(\w*)(?:\(([\w$.\-*/ ]*)\))?: (.*)$/, // 自定义提交信息头部的解析规则
+    headerCorrespondence: ['type', 'scope', 'subject'], // 映射头部解析结果到这些字段
+    noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'], // 捕获破坏性变更的关键字
+  },
+  // 自定义生成器选项
+  writerOpts: {
+    transform: (commit) => {
+      let type = commit.type
+      if (type === 'feat') {
+        commit.type = 'Features'
+      } else if (type === 'fix') {
+        commit.type = 'Bug Fixes'
+      } else if (type === 'perf') {
+        commit.type = 'Performance Improvements'
+      } else if (type === 'revert') {
+        commit.type = 'Reverts'
+      } else if (!type || type === 'chore') {
+        return
+      }
+      return commit
+    },
+    groupBy: 'type', // 按照类型组织提交
+    commitGroupsSort: 'title', // 按标题排序提交组
+    commitsSort: ['scope', 'subject'], // 在组内按范围和主题排序提交
+  },
+}
